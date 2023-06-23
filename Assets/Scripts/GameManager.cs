f0a1b5626959c588618abfb0b4a4bc9d43a330f8
ansariumer <ansariumerr@gmail.com>
2023-06-23 16:01:41 +0530

    add point collection and camera follow feature

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,20 +1,183 @@
-using System.Collections;
-using System.Collections.Generic;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class GameManager : MonoBehaviour
+// {
+//     #region Singleton class: GameManager 
+
+//     public static GameManager Instance;
+
+//     void Awake ()
+//     {
+//         if (Instance == null)
+//         {
+//             Instance = this; 
+//         }
+//     }
+
+//     #endregion
+
+//     Camera cam;
+
+//     public Player player;
+//     public Trajectory trajectory;
+
+//     [SerializeField]
+//     float pushForce = 4f;
+
+//     bool isDragging = false;
+
+//     Vector2 startPoint;
+//     Vector2 endPoint;
+//     Vector2 direction;
+//     Vector2 force;
+//     float distance;
+
+
+//     // -------------------------------------
+//     void Start()
+//     {
+//         cam = Camera.main;
+//         player.DesactivateRb ();
+//     }
+
+//     void Update()
+//     {
+//         if (Input.GetMouseButtonDown (0))
+//         {
+//             isDragging = true;
+//             OnDragStart ();
+//         }
+//         if (Input.GetMouseButtonUp (0))
+//         {
+//             isDragging = false;
+//             OnDragEnd ();
+//         }
+//         if (isDragging)
+//         {
+//             OnDrag ();
+//         }
+//     }
+
+//     // ---Drag---------------------------
+//     void OnDragStart ()
+//     {
+//         player.DesactivateRb ();
+//         startPoint = cam.ScreenToWorldPoint (Input.mousePosition);
+
+//         trajectory.Show ();
+//     }
+
+//     void OnDrag ()
+//     {
+//         endPoint = cam.ScreenToWorldPoint (Input.mousePosition);
+//         distance = Vector2.Distance (startPoint, endPoint);
+//         direction = (startPoint - endPoint).normalized;
+//         force = direction * distance * pushForce;
+
+//         // just for debug 
+//         Debug.DrawLine (startPoint, endPoint); 
+
+//         trajectory.UpdateDots(player.pos, force);
+//     }
+
+//     void OnDragEnd ()
+//     {
+//         // push the player 
+//         player.ActivateRb ();
+//         player.Push (force);
+
+//         trajectory.Hide ();
+//     }
+
+// }
+﻿
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    #region Singleton class: GameManager 
+	#region Singleton class: GameManager
 
-    public static GameManager Instance;
+	public static GameManager Instance;
 
-    void Awake ()
-    {
-        if (Instance == null)
-        {
-            Instance = this; 
-        }
-    }
+	void Awake ()
+	{
+		if (Instance == null) {
+			Instance = this;
+		}
+	}
 
-    #endregion
+	#endregion
+
+	Camera cam;
+
+	public Player player;
+	public Trajectory trajectory;
+	[SerializeField] float pushForce = 4f;
+
+	bool isDragging = false;
+
+	Vector2 startPoint;
+	Vector2 endPoint;
+	Vector2 direction;
+	Vector2 force;
+	float distance;
+
+	//---------------------------------------
+	void Start ()
+	{
+		cam = Camera.main;
+		player.DesactivateRb ();
+	}
+
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown (0)) {
+			isDragging = true;
+			OnDragStart ();
+		}
+		if (Input.GetMouseButtonUp (0)) {
+			isDragging = false;
+			OnDragEnd ();
+		}
+
+		if (isDragging) {
+			OnDrag ();
+		}
+	}
+
+	//-Drag--------------------------------------
+	void OnDragStart ()
+	{
+		player.DesactivateRb ();
+		startPoint = cam.ScreenToWorldPoint (Input.mousePosition);
+
+		trajectory.Show ();
+	}
+
+	void OnDrag ()
+	{
+		endPoint = cam.ScreenToWorldPoint (Input.mousePosition);
+		distance = Vector2.Distance (startPoint, endPoint);
+		direction = (startPoint - endPoint).normalized;
+		force = direction * distance * pushForce;
+
+		//just for debug
+		Debug.DrawLine (startPoint, endPoint);
+
+
+		trajectory.UpdateDots (player.pos, force);
+	}
+
+	void OnDragEnd ()
+	{
+		//push the player
+		player.ActivateRb ();
+
+		player.Push (force);
+
+		trajectory.Hide ();
+	}
+
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,12 +10,12 @@
 //     public CircleCollider2D col;
 
 //     [HideInInspector]
-//     public Vector3 HumanPos{ get { return transform.position}}
+//     public Vector3 pos { get { return transform.position; } }
 
 //     void Awake()
 //     {
-//         rb = GetComponent<Rigidbody2D>();
-//         col = GetComponent<CircleCollider2D>();
+//         rb = GetComponent<Rigidbody2D> ();
+//         col = GetComponent<CircleCollider2D> ();
 //     } 
 
 //     public void Push (Vector3 force)
@@ -30,8 +30,42 @@
 
 //     public void DesactivateRb()
 //     {
-//         rb.velocity = Vector3.Zero;
+//         rb.velocity = Vector3.zero;
 //         rb.angularVelocity = 0f;
 //         rb.isKinematic = true;
 //     }
 // }
+﻿
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	[HideInInspector] public Rigidbody2D rb;
+	[HideInInspector] public CircleCollider2D col;
+
+	[HideInInspector] public Vector3 pos { get { return transform.position; } }
+
+	void Awake ()
+	{
+		rb = GetComponent<Rigidbody2D> ();
+		col = GetComponent<CircleCollider2D> ();
+	}
+
+	public void Push (Vector2 force)
+	{
+		rb.AddForce (force, ForceMode2D.Impulse);
+	}
+
+	public void ActivateRb ()
+	{
+		rb.isKinematic = false;
+	}
+
+	public void DesactivateRb ()
+	{
+		rb.velocity = Vector3.zero;
+		rb.angularVelocity = 0f;
+		rb.isKinematic = true;
+	}
+}
+
